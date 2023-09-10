9d18127e0fd0eee0c72a164b55bb70e964605da6
ojagggyo <ojagggyo@gmail.com>
2023-09-10 16:16:58 +0900

    m

--- javascript/get_followers.js
+++ javascript/get_followers.js
@@ -1,23 +1,10 @@
 const dsteem = require('dsteem')
 const client = new dsteem.Client('https://api.steememory.com');
 
-
-module.exports.getFollowersSync = async (username) => {
-            
-    //NG client.call('follow_api','get_followers',{account:username,start:'',type:'blog',limit:10}).then(
-    client.call('follow_api','get_followers',[username, "", "blog", 10]).then(
-        function(result) {
-            console.log("mainAsync OK", JSON.stringify(result));  
-        },
-        function(error) {
-            console.log("mainAsync error", error);
-        }
-    )
-};
-
 module.exports.getFollowers = async (username) => {
     return new Promise((resolve, reject) => {
 		(async() => {
+            //NG client.call('follow_api','get_followers',{account:username,start:'',type:'blog',limit:10}).then(
             client.call('follow_api','get_followers',[username, "", "blog", 10]).then(
                 function(result) {
                     resolve(result)
@@ -34,7 +21,7 @@ module.exports.getFollowers = async (username) => {
 async function mainSync(acount_name){ 
     try {
         const ret = await module.exports.getFollowers(acount_name)
-        console.log("mainSync OK", JSON.stringify(ret))
+        console.log("★★★", JSON.stringify(ret))
     } catch (e) {
         console.log("mainSync error",JSON.stringify(e))
     }
@@ -44,7 +31,7 @@ async function mainSync(acount_name){
 async function mainAsync(acount_name){ 
     module.exports.getFollowers(acount_name).then(
         (data) => {
-          console.log("mainAsync OK", JSON.stringify(data));
+          console.log("☆☆☆", JSON.stringify(data));
         },
         (e) => {
           console.log("mainAsync error", e);
