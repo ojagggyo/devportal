ba3058363fadc9dba9ab2933131bdf7bac4fbf14
ojagggyo <ojagggyo@gmail.com>
2023-09-10 10:41:27 +0900

    m

--- find_rc_accounts.js
+++ find_rc_accounts.js
@@ -1,17 +1,11 @@
 const dsteem = require('dsteem')
 const client = new dsteem.Client('https://api.steememory.com');
 
-// curl -s --data '{"jsonrpc":"2.0", 
-// "method":"rc_api.find_rc_accounts", 
-// "params":{"accounts":["yasu"]}, "id":1}' 
-
-//client.database.call('get_state', [query]).then(result => {
-
 module.exports.findRCAccounts = async (username) => {
     
-    client.rc.call('find_rc_accounts', {"accounts":["yasu"]} ).then(
+    client.rc.call('find_rc_accounts', {"accounts":[username]} ).then(
         function(result) {
-            console.log(result[0]);
+            console.log(JSON.stringify(result));
         },
         function(error) {
             console.error(error);
