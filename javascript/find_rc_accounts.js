ca80e275e603e8f7d7d9c9c9e3986509c8fc739e
ojagggyo <ojagggyo@gmail.com>
2023-09-10 12:34:01 +0900

    m

--- javascript/find_rc_accounts.js
+++ javascript/find_rc_accounts.js
@@ -2,17 +2,43 @@ const dsteem = require('dsteem')
 const client = new dsteem.Client('https://api.steememory.com');
 
 module.exports.findRCAccounts = async (username) => {
-    
-    client.call('rc_api','find_rc_accounts', {"accounts":[username]} ).then(
-        function(result) {
-            console.log(JSON.stringify(result));
-        },
-        function(error) {
-            console.error(JSON.stringify(error));
-        }
-    )
+    return new Promise((resolve, reject) => {
+		(async() => {
+            client.call('rc_api','find_rc_accounts', {"accounts":[username]} ).then(
+                function(result) {
+                    resolve(result)
+                },
+                function(error) {
+                    reject(error)
+                }
+            )
+        })();
+    });
 };
 
- // 呼び出し方法
- this.findRCAccounts("yasu")
+// 同期
+async function mainSync(acount_name){ 
+    try {
+        const ret = await module.exports.findRCAccounts(acount_name)
+        console.log("mainSync OK", JSON.stringify(ret))
+    } catch (e) {
+        console.log("mainSync error",JSON.stringify(e))
+    }
+ }
+
+// 非同期
+async function mainAsync(acount_name){ 
+    module.exports.findRCAccounts(acount_name).then(
+        (data) => {
+          console.log("mainAsync OK", JSON.stringify(data));
+        },
+        (e) => {
+          console.log("mainAsync error", JSON.stringify(e));
+        }
+    )
+ }
+ 
+// 呼び出し方法
+mainAsync("yasu")
+mainSync("yasu.witness")
  
