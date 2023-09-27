5031b3e61ea456ed9cec6e97a9e139a310ce3e58
ojagggyo <ojagggyo@gmail.com>
2023-09-27 21:04:27 +0900

    m

--- javascript/app.js
+++ javascript/app.js
@@ -31,7 +31,7 @@ xxx.callAsync("condenser_api","get_witnesses_by_vote", ["",107]).then(
             if (witness.owner != "yasu.witness"){
                 continue
             }
-            console.log(index+1,witness.owner)
+            console.log(index+1,witness.owner,witness.votes)
         }
     },
     function(error) {
